057149e7c95774d9457d30bcd0b14e3cf3eb1120
igwe9 <emmaigwesi@gmail.com>
2020-09-18 16:20:09 +0100

    initial commit

--- POST/urls.py
+++ POST/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='Home'),
-    path('blog/', views.blog, name='blog'),
     path('next/', views.next, name='next'),
     path('Add_post', views.Add_post, name='Add_post'),
     path('<slug:slug>/', views.detail, name='detail'),
